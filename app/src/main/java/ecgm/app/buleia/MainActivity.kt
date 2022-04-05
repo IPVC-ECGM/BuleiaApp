da29694bd6e12a63f273897b188a32f3a3f5c27b
Filipe <carloscz@ipvc.pt>
2022-04-05 15:15:30 +0100

    Update

--- app/src/main/java/ecgm/app/buleia/MainActivity.kt
+++ app/src/main/java/ecgm/app/buleia/MainActivity.kt
@@ -1,11 +1,20 @@
 package ecgm.app.buleia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Esconder Barra do menu
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+        },3000)
     }
 }
