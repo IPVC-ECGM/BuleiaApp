cd8201c5258294b516b2e7a388a00ae022468562
Filipe <carloscz@ipvc.pt>
2022-04-20 23:37:29 +0100

    Constantes Publicas Nome da app Icon

---
+++ app/src/main/java/ecgm/app/buleia/Constantes.java
@@ -0,0 +1,7 @@
+package ecgm.app.buleia;
+
+public class Constantes {
+    public static int Screen_Width;
+    //Altura do ecrâ
+    public static int Screen_Height;
+}
--- app/src/main/java/ecgm/app/buleia/MainActivity.kt
+++ app/src/main/java/ecgm/app/buleia/MainActivity.kt
@@ -1,9 +1,11 @@
 package ecgm.app.buleia
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +14,18 @@ class MainActivity : AppCompatActivity() {
         //Esconder Barra do menu
         supportActionBar?.hide()
 
+        //Pegar nas dimensões do ecrâ
+        //Criar metricas do ecrâ
+        val dm = DisplayMetrics()
+        //buscar medidas do ecrâ utilizado
+        windowManager.defaultDisplay.getMetrics(dm)
+        //Gravar os valores nas constantes da classe constantes
+        Constantes.Screen_Width = dm.widthPixels
+        Constantes.Screen_Height = dm.heightPixels
+
         Handler().postDelayed({
             val intent = Intent(this@MainActivity, HomeActivity::class.java)
             startActivity(intent)
-        },3000)
+        },15000)
     }
 }
