991463e0dd7f958e199c7144fbed8738293bc089
kanzaky <kanzakycloud@gmail.com>
2022-05-04 17:18:10 +0100

    MENU ADD

--- app/src/main/java/ecgm/app/buleia/Perfil.kt
+++ app/src/main/java/ecgm/app/buleia/Perfil.kt
@@ -3,25 +3,55 @@ package ecgm.app.buleia
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import ecgm.app.buleia.databinding.ActivityPerfilBinding
 
 class Perfil : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     private lateinit var binding: ActivityPerfilBinding
-
     private lateinit var actionBar: ActionBar
-
     private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         actionBar = supportActionBar!!
         actionBar.title = "Profile"
+
+        drawerLayout = findViewById(R.id.drawerlayout)
+        val menu1 : NavigationView = findViewById(R.id.menu_1)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        menu1.setNavigationItemSelectedListener {
+            it.isChecked = true
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment(),it.title.toString())
+                R.id.history -> Toast.makeText(applicationContext,"clicked History",Toast.LENGTH_SHORT).show()
+                R.id.notificacao -> Toast.makeText(applicationContext,"clicked notificacao",Toast.LENGTH_SHORT).show()
+                R.id.definicao -> Toast.makeText(applicationContext,"clicked definicao",Toast.LENGTH_SHORT).show()
+                R.id.sair -> Toast.makeText(applicationContext,"clicked sair",Toast.LENGTH_SHORT).show()
+
+            }
+            true
+
+        }
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
@@ -30,6 +60,23 @@ class Perfil : AppCompatActivity() {
             firebaseAuth.signOut()
             checkUser()
         }
+    }
+
+    private  fun replaceFragment(fragment: Fragment, title:String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun checkUser() {
