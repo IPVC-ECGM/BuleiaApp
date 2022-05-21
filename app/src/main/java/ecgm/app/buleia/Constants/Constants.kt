e7b27958c142e88cd28966843f9fded832dd964f
Filipe <carloscz@ipvc.pt>
2022-05-21 01:09:43 +0100

    Final splash screen

--- app/src/main/java/ecgm/app/buleia/Constants/Constants.kt
+++ app/src/main/java/ecgm/app/buleia/Constants/Constants.kt
@@ -5,10 +5,8 @@ class Constants {
         const val BASE_URL = "https://fcm.googleapis.com"
         const val SERVER_KEY = "AAAAqXYqpMA:APA91bEVaP3o4VgwQZzDd9Rv-Tjsa3F05RLVQHhohtDU21fXMrzU5NNyOR4izskiprAjoW6oq9tf4vAPMgiE_aSf67VPQeQxfhyJWXM4mskIohNPE7pSkpYkL9jMjvDekZ3-jQW5enJG" // get firebase server key from firebase project setting
         const val CONTENT_TYPE = "application/json"
+
+
     }
 
-    //Largura do ecrâ
-    var Screen_Width = 0
-    //Altura do ecrâ
-    var Screen_Height = 0
 }
