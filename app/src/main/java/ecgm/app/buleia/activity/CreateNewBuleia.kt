7886eb9edd822f12f0fd2e8cbbe49b3e9799205c
Filipe <carloscz@ipvc.pt>
2022-05-23 03:15:21 +0100

    Testing

--- app/src/main/java/ecgm/app/buleia/activity/CreateNewBuleia.kt
+++ app/src/main/java/ecgm/app/buleia/activity/CreateNewBuleia.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.FirebaseError
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
